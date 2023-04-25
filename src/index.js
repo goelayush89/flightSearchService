5a92da220ae33d92622909b045fc31fea188bbe4
ayush1729cpp <75876302+ayush1729cpp@users.noreply.github.com>
2023-04-26 00:48:32 +0530

    router added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig');
+const ApiRoutes = require('./routes/index');
 
 
 
@@ -9,7 +10,7 @@ const setupAndStartServer = async () => {
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
-
+    app.use('/api', ApiRoutes);
     app.listen(PORT, () => {
         console.log(`server is working on ${PORT} `);
     })
